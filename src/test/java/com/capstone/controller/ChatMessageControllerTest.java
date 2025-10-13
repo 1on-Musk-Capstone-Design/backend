b5eb70095f6652422e1b030ddf7e5f36646282ad
Jeong Yang Hyo <jyhyo2002@gmail.com>
2025-10-13 14:12:59 +0900

    chore: 코드 정리 및 포맷팅

--- src/test/java/com/capstone/controller/ChatMessageControllerTest.java
+++ src/test/java/com/capstone/controller/ChatMessageControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
