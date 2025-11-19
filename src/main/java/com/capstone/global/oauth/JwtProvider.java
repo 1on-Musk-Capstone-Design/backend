ebf8ebe5827a89b9f07774bf4a96f4defee49b2e
Hyun-jun-Lee0811 <korong0811@naver.com>
2025-11-20 01:45:02 +0900

    refactor: JwtProvider getUserIdFromAccessToken parseClaimsJws 적용

--- src/main/java/com/capstone/global/oauth/JwtProvider.java
+++ src/main/java/com/capstone/global/oauth/JwtProvider.java
@@ -46,8 +46,8 @@ public class JwtProvider {
     Claims claims = Jwts.parser()
         .setSigningKey(getSigningKey())
         .build()
-        .parseSignedClaims(token)
-        .getPayload();
+        .parseClaimsJws(token)
+        .getBody();
 
     return claims.get("user_id", Long.class);
   }
